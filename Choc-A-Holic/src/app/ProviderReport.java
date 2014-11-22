e9ccfb6d98590dfa8ee9cee64d0d4b03b7982da7
RemingtonJames <rkonarsk@pio.carrollu.edu>
2014-11-22 14:04:19 -0600

    deleted metadata

--- Choc-A-Holic/src/app/ProviderReport.java
+++ Choc-A-Holic/src/app/ProviderReport.java
@@ -13,19 +13,15 @@ public class ProviderReport implements IReport {
 	@Override
 	public void setMemeber(Member m) {
 		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public void setProvider(Provider p) {
 		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public void setProviderService(ProvidedService ps) {
 		// TODO Auto-generated method stub
-
 	}
-
 }
