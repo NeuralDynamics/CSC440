771aef83d53fe7e98448fc25d8d7e28b51709905
RemingtonJames <rkonarsk@pio.carrollu.edu>
2014-11-22 12:47:33 -0600

    Added IReport methods & Report Base Classes

---
+++ Choc-A-Holic/src/app/EFTRecordsReport.java
@@ -0,0 +1,29 @@
+package app;
+
+public class EFTRecordsReport implements IReport {
+
+	@Override
+	public void runReport(String filename) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setMemeber(Member m) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setPRovider(Provider p) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setProviderService(ProvidedService ps) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
