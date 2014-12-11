522756046acda1c27a0f52f3c55de60566cc9b17
RemingtonJames <RemingtonJames@users.noreply.github.com>
2014-12-10 19:57:54 -0600

    added Misc class for random generic methods that could be needed elsewhere

--- Choc-A-Holic/src/app/RptEFTRecords.java
+++ Choc-A-Holic/src/app/RptEFTRecords.java
@@ -1,7 +1,5 @@
 package app;
 
-import java.io.*;
-import java.text.*;
 import java.util.*;
 
 public class RptEFTRecords extends AReport {
@@ -9,27 +7,34 @@ public class RptEFTRecords extends AReport {
 	@Override
 	protected void buildRecords() {				
 		String output = "";
-		
+
 		// Allocate space for the queue
 		allocateQueue(1);
+		// Loop through all of the Provided Service records
+		while (providedSvc.isEmpty() == false) {
+			
+			// Initialize the record
+			output = "";
+			ProvidedService ps = providedSvc.poll();
 		
-		// Write out the current Date/Time
-		output += dtFormat_DtTm.format(new Date()) + delimiter;
-		
-		// Write out the Date of the Service
-		output += dtFormat_Dt.format(providedSvc.getDateOfService()) + delimiter;
-				
-		// Write out the Provider Number
-		output += String.format(Locale.US, "%09d", providedSvc.getProviderNumber()) + delimiter;
-		
-		// Write out the Member Number
-		output += String.format(Locale.US, "%09d", providedSvc.getMemberNumber()) + delimiter;
-		
-		// Write out the Member Number
-		output += String.format(Locale.US, "%06d", providedSvc.getServiceCode()) + delimiter;
-		
-		// Write out the Comments
-		output += providedSvc.getComments().PadRight(100).SubString(0, 100);
+			// Write out the current Date/Time
+			output += dtFormat_DtTm.format(new Date()) + delimiter;
+			
+			// Write out the Date of the Service
+			output += dtFormat_Dt.format(ps.getDateOfService()) + delimiter;
+					
+			// Write out the Provider Number
+			output += String.format(Locale.US, "%09d", ps.getProviderNumber()) + delimiter;
+			
+			// Write out the Member Number
+			output += String.format(Locale.US, "%09d", ps.getMemberNumber()) + delimiter;
+			
+			// Write out the Service Code
+			output += String.format(Locale.US, "%06d", ps.getServiceCode()) + delimiter;
+			
+			// Write out the Comments
+			output += Misc.padRight(ps.getComments(), 100);
+		}
 		
 		// Queue up the record
 		enqueueRecord(output);
