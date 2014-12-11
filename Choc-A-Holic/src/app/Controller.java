457f29ef862ba5f442cb53947ea6fed9216bf8c7
RemingtonJames <RemingtonJames@users.noreply.github.com>
2014-12-10 19:11:25 -0600

    Start of JUnit Tests

--- Choc-A-Holic/src/app/Controller.java
+++ Choc-A-Holic/src/app/Controller.java
@@ -1,13 +1,36 @@
 package app;
 
 public class Controller {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	private MemberManager mm = null;
+	private ServicesManager sm = null;
+	private ProviderManager pm = null;
+	
+	public void initialize() {
+		
+		// Create the manager instances
+		mm = new MemberManager();
+		sm = new ServicesManager();
+		pm = new ProviderManager();
 	}
-
+	
+	public void save() {
+		try {
+			mm.save();
+		} catch (Exception Ex) {
+			System.out.println("Error saving Member Data");
+		}
+		
+		try {
+			sm.save();
+		} catch (Exception Ex) {
+			System.out.println("Error saving Service Data");
+		}
+		
+		try {
+			pm.save();
+		} catch (Exception Ex) {
+			System.out.println("Error saving Provider Data");
+		}
+	}
 }
